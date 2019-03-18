3686886e4392d87930fa1e1794bf570c86c050ad
Steve Gale <sgale@gordontafe.edu.au>
2019-03-18 16:44:41 +1100

    data base creation template

---
+++ php/createI4db.php
@@ -0,0 +1,22 @@
+<?php
+$servername = "localhost";
+$username = "I40";
+$password = "Password1";
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password);
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+// Create database
+$sql = "CREATE DATABASE StevesSipyIOTdb";
+if (mysqli_query($conn, $sql)) {
+    echo "Industry 4.0 Database created successfully";
+} else {
+    echo "Error creating Industry 4.0 database: " . mysqli_error($conn);
+}
+
+mysqli_close($conn);
+?>
